c9126257c6419cd91a68e5c9fb40d3d99e9d3861
Neha Deshmukh <nddesh1@gmail.com>
2020-02-05 22:24:20 -0800

    adding converters assignment

--- des157-wi20/js/script.js
+++ des157-wi20/js/script.js
@@ -1,41 +1,97 @@
-//Deshmukh, Neha
-//11-04-19
-//Overlapping Circles
-
-color a = color(random(0, 255), random(0, 255), random(0, 255));
-color b = color(random(0, 255), random(0, 255), random(0, 255));
+color penColor = color(255, 255, 255);
+color a = color(255, 255, 255); //white
+color b = color(255, 0, 0); //red
+color c = color(0, 255, 0); //green
+color d = color(0, 0, 255); //blue
+color e = color(250, 250, 0); //yellow
 
 function setup() {
   var myCanvas = createCanvas(800, 250);
   background(0);
-  myCanvas.parent(mySketch);
 }
 
 function draw() {
-  for(var y = 100; y < height+100; y += 200) {  //first set
-    for(var x = 100; x < width+100; x += 200) {  //duplicate for multiple columns
-      drawCirc(x, y, a);
+  //white button
+  noStroke();
+  fill(a);
+  rect(680, 50, 50, 40);
+
+  //red button
+  noStroke();
+  fill(b);
+  rect(680, 90, 50, 40);
+
+  //green button
+  noStroke();
+  fill(c);
+  rect(680, 130, 50, 40);
+
+  //blue button
+  noStroke();
+  fill(d);
+  rect(740, 50, 50, 40);
+
+  //yellow button
+  noStroke();
+  fill(e);
+  rect(740, 90, 50, 40);
+
+  //clear board when key pressed
+  stroke(255);
+  strokeWeight(1);
+  fill(0);
+  rect(740, 130, 50, 40);
+  fill(255);
+  textSize(15);
+  text("CLEAR", 742, 130);
+
+  //mouse movement
+  if (mouseIsPressed) {
+    if (mouseX > 680 && mouseX < 720) {
+      //white button
+      if (mouseY > 50 && mouseY < 90) {
+        penColor = a;
+      }
+
+      //red button
+      else if (mouseY > 90 && mouseY < 130) {
+        penColor = b;
+      }
+
+      //green button
+      else if (mouseY > 130 && mouseY < 170) {
+        penColor = c;
+      }
+    }
+
+
+    if (mouseX > 740 && mouseX < 790) {
+      //color buttons
+
+      //blue button
+      else if (mouseY > 210 && mouseY < 250) {
+        penColor = d;
+      }
+
+      //yellow button
+      else if (mouseY > 250 && mouseY < 290) {
+        penColor = e;
+      }
+
+      else if (mouseY > 300 && mouseY < 340) {
+        background(0);
+      }
     }
   }
 
-  for(var y = 0; y < height+100; y += 200) {  //second overlapping set
-    for(var x = 0; x < width+100; x += 200) {  //duplicate for multiple columns
-      drawCirc(x, y, b);
+  //draw only when mouse is pressed
+  if (mouseIsPressed) {
+    if (mouseX > 0 && mouseX < 500) {
+      if (mouseY > 0 && mouseY < 500) {
+          stroke(penColor);
+          strokeWeight(2);
+          line(mouseX, mouseY, pmouseX, pmouseY);
+      }
     }
-  }
-}
-
-function drawCirc(var x, var y, color c) {
-    noFill();
-    stroke(c);
-    ellipse(x, y, 200, 200);
-}
-
-
-//change color when enter is pressed
-function mouseIsPressed() {
-    background(0);
-    a = color(random(0, 255), random(0, 255), random(0, 255));
-    b = color(random(0, 255), random(0, 255), random(0, 255));
   }
 }
